0770e688c8a629adcd172565ce93a533578ff803
Rushikesh Bagade <rbagade911@gmail.com>
2024-11-27 23:24:48 +0530

    upddate dist folder

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const cors = require('cors')
 const connect = require('./dbConfig')
+const path = require("path");
+
 
 const app = express()
 app.use(cors())
@@ -12,8 +14,16 @@ const userRoutes = require("./routes/user")
 
 app.use('/user', userRoutes)
 
-app.get("/",(req,res)=>{
-    res.send("Hello")
-})
+const DIST_FOLDER = path.join(__dirname, "dist");
+app.use(express.static(DIST_FOLDER));
+
+// Fallback to serve index.html for SPA (Single Page Application)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(DIST_FOLDER, "index.html"));
+});
+
+// app.get("/",(req,res)=>{
+//     res.send("Hello")
+// })
 
 app.listen(5000)
