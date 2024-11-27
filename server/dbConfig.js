64be9a2cde79f8f2a74db673006cb94cc4803db1
Rushikesh Bagade <rbagade911@gmail.com>
2024-11-27 22:51:48 +0530

    added env file and created dist

--- server/dbConfig.js
+++ server/dbConfig.js
@@ -1,8 +1,9 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
 
 const connect = async()=>{
     try {
-        const connection = await mongoose.connect("mongodb+srv://rishi:nrarVWGs6IJLpADk@cluster0.sirc6hl.mongodb.net/adminDashboard?retryWrites=true&w=majority")
+        const connection = await mongoose.connect(process.env.DB)
         console.log("Connected");
         
     } catch (error) {
